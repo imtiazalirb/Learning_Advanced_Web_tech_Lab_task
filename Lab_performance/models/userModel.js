0527e0fd095423c0da034b27414681c506a3554d
Imtiaz <imtiazali4090@gmail.com>
2020-11-10 12:41:00 +0600

    removed unnecessary files

--- Lab_performance/models/userModel.js
+++ Lab_performance/models/userModel.js
@@ -4,7 +4,7 @@ module.exports= {
 	validate: function(user, callback){
 		var sql = "select * from users where username='"+user.username+"' and password='"+user.password+"'";
 		db.getResults(sql, function(results){
-			if(results.length >0 ){
+			if(results.length>0){
 				callback(results[0].type);
 			}
 		});
